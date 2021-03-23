9f00962a25fcd01e1e61c236c46ca409bc39e161
ndkv9 <vu.nguyen101@gmail.com>
2021-03-23 12:37:46 +0200

    use static queries inside a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+
 export default function Home() {
   return (
     <Layout>
