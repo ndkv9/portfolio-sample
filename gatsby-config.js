76df544394ef20b006b1138c2547aaefae3e3645
ndkv9 <vu.nguyen101@gmail.com>
2021-03-24 10:27:19 +0200

    setup transform remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `notes`,
-        path: `${__dirname}/src/notes/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
       },
