740757350962c86acd1c7bc5b0cb246209890790
ndkv9 <vu.nguyen101@gmail.com>
2021-03-25 12:15:09 +0200

    add page's content

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,15 +1,41 @@
+import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.modul.css"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
